33731a0dc141a555d810be3b73e14f9d37a24138
wk-Nemo <2181544173@qq.com>
2021-12-24 22:14:10 +0800

    fix: url fix

--- blog-frontend/src/server/path.js
+++ blog-frontend/src/server/path.js
@@ -1,2 +1,2 @@
-export const ONLINE = '47.103.200.75:8081/'
-export const LOCAL = 'localhost:8081/'
+export const ONLINE = 'http://47.103.200.75:8081/'
+export const LOCAL = 'http://localhost:8081/'
