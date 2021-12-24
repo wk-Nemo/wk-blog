b07bfc3297cf5b254140d39d07b8fd1887ab5444
wk-Nemo <2181544173@qq.com>
2021-12-24 23:11:44 +0800

    fix

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
 connection.connect()
 
 //解决跨域
-app.all('*',function (req, res, next) {
+app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
--- blog-frontend/src/server/path.js
+++ blog-frontend/src/server/path.js
@@ -1,2 +1,2 @@
-export const ONLINE = 'http://47.103.200.75:8081/'
+export const ONLINE = 'http://47.103.200.75:81/'
 export const LOCAL = 'http://localhost:8081/'
