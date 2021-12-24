62979ed431b4910f992981279d35bf1877fb6b65
wk-Nemo <2181544173@qq.com>
2021-12-24 22:39:31 +0800

    feat: port change

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -26,7 +26,7 @@ app.all('*',function (req, res, next) {
 
 console.log("success1")
 
-var server = app.listen(8081, function () {
+var server = app.listen(80, function () {
  
   var host = server.address().address
   var port = server.address().port
