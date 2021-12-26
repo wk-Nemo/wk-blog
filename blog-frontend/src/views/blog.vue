75411ca7681a5f156abcbfa5b581257f140eb171
wk-Nemo <2181544173@qq.com>
2021-12-26 17:38:17 +0800

    feat: mode change

--- blog-frontend/src/views/blog.vue
+++ blog-frontend/src/views/blog.vue
@@ -76,7 +76,7 @@ export default {
       if (newVal) {
         require('@/assets/scss/github-markdown-dark.css')
       } else {
-        require('@/assets/scss/github-markdown.css')
+        require('@/assets/scss/github-markdown-light.css')
       }
     }
   },
@@ -101,7 +101,7 @@ export default {
     if (this.darkMode) {
       require('@/assets/scss/github-markdown-dark.css')
     } else {
-      require('@/assets/scss/github-markdown.css')
+      require('@/assets/scss/github-markdown-light.css')
     }
   }
 }
@@ -269,16 +269,13 @@ export default {
         color: #fff;
       }
     }
-    /* .markdown-body {
+    .markdown-body {
       background-color: rgb(44, 50, 60);
       color: white;
       pre {
-        background-color: #24292f;
+        background-color: #24292f !important;
       }
     }
-    .markdown-body pre {
-      background-color: rgb(31, 34, 39) !important;
-    } */
   }
   .blog-footer .lite-footer .back {
     background: url('https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic3-nc.pocoimg.cn%2Fimage%2Fpoco%2Fworks%2F10%2F2020%2F0903%2F00%2F15990627524040719_201497536_H1920.jpg&refer=http%3A%2F%2Fpic3-nc.pocoimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1643081810&t=58ec63178a3be86d4cf0095492de143a') 50% center / cover no-repeat fixed rgb(255, 255, 255);
