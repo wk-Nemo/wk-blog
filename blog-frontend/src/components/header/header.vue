84d8ce4af01068ad563761e3140b7ae18fcdbf2b
wk-Nemo <2181544173@qq.com>
2021-12-26 12:24:03 +0800

    fix: ios bug

--- blog-frontend/src/components/header/header.vue
+++ blog-frontend/src/components/header/header.vue
@@ -72,8 +72,7 @@ export default {
 
 .dark {
   .header-banner {
-    background: rgb(255, 255, 255) url('https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic3-nc.pocoimg.cn%2Fimage%2Fpoco%2Fworks%2F10%2F2020%2F0903%2F00%2F15990627524040719_201497536_H1920.jpg&refer=http%3A%2F%2Fpic3-nc.pocoimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1643081810&t=58ec63178a3be86d4cf0095492de143a') 50% center / cover no-repeat fixed;
-    background-size: 100% 99.9%;
+    background: url(https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic3-nc.pocoimg.cn%2Fimage%2Fpoco%2Fworks%2F10%2F2020%2F0903%2F00%2F15990627524040719_201497536_H1920.jpg&refer=http%3A%2F%2Fpic3-nc.pocoimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1643081810&t=58ec63178a3be86d4cf0095492de143a) no-repeat center/cover;
   }
 }
 
