86ce14eddcb29612c475e0b9132816dacb60027f
wk-Nemo <2181544173@qq.com>
2022-02-27 22:20:40 +0800

    feat: page tunner

--- blog-frontend/src/components/blog-list/page-turner/page-tuner.vue
+++ blog-frontend/src/components/blog-list/page-turner/page-tuner.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="page-tuner">
     <div
-      class="tuner-item"
-      :class="{'tuner-item-active': page === nowPage}"
       @click="clickItem(page)"
-      v-for="page in pages"
+      v-for="page in showPages"
       :key="page"
     >
-      {{page + 1}}
+      <div
+        v-if="typeof  page ===  'number'"
+        class="tuner-item"
+        :class="{'tuner-item-active': page === nowPage}"
+      >
+        {{page + 1}}
+      </div>
+      <div v-else class="ellipsis">
+        {{page}}
+      </div>
     </div>
   </div>
 </template>
@@ -29,10 +36,34 @@ export default {
       nowPage: 0
     }
   },
+  computed: {
+    showPages () {
+      const pages = []
+      const length = this.pages.length
+      const nowPage = this.nowPage
+
+      // 5页以内正常展示
+      if (length <= 5) {
+        return this.pages
+      } else {
+        if (this.nowPage <= 1) {
+          pages.push(0, 1, 2, '...', length - 1)
+        } else if (this.nowPage >= length - 2) {
+          pages.push(0, '...', length - 3, length - 2, length - 1)
+        } else {
+          pages.push(0, '...', nowPage - 1, nowPage, nowPage + 1, '...', length - 1)
+        }
+      }
+
+      return pages
+    }
+  },
   methods: {
     clickItem (page) {
-      this.nowPage = page
-      this.$emit('changePage', page)
+      if (typeof page === 'number') {
+        this.nowPage = page
+        this.$emit('changePage', page)
+      }
     }
   }
 }
@@ -52,6 +83,7 @@ export default {
     border-radius: 50%;
     line-height: 2.4rem;
     text-align: center;
+    transition: all 0.6s;
   }
   .tuner-item:hover {
     background-color: rgb(42, 43, 51);
@@ -62,6 +94,9 @@ export default {
     background-color: rgb(42, 43, 51);
     background-image: none;
     color: #fff;
+  }
+  .ellipsis {
+    margin: 0 5px;
   }
 }
 </style>
