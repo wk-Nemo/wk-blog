d9b254360014d17695a6de8ad0878ed0245b7bd0
wukui04 <wukui04@meituan.com>
2022-09-27 16:50:22 +0800

    del: 删除底部

--- blog-frontend/src/App.vue
+++ blog-frontend/src/App.vue
@@ -2,19 +2,16 @@
   <div class="app">
     <header-nav />
     <router-view :key="routePath"></router-view>
-    <Footer />
   </div>
 </template>
 
 <script>
 import HeaderNav from '@/components/header-nav/header-nav'
-import Footer from '@/components/footer/footer'
 
 export default {
   name: 'app',
   components: {
-    HeaderNav,
-    Footer
+    HeaderNav
   },
   computed: {
     routePath () {
